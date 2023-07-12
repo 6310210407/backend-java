1f6656ca3a1697523b8f4f8e041ed50779d4f228
6310210407 <6310210407@email.psu.ac.th>
2023-07-12 12:09:14 +0700

    lab2 getAll, Create Employee

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -10,6 +10,8 @@ public class BackendJavaApplication {
 		
 		SpringApplication.run(BackendJavaApplication.class, args);
 		System.out.println("Hello sping");
+	    System.out.println("Sping boot");
+	    
 	}
 	
 
